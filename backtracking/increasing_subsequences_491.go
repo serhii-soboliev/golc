ab359f21cf1c2e3f7b9fe6d69e93d7b78150ef8b
sergiy.sobolev@gmail.com <sergiy.sobolev@gmail.com>
2022-07-13 02:15:43 +0300

    [491. Increasing Subsequences] Recursive Backtracking solution

--- backtracking/increasing_subsequences_491.go
+++ backtracking/increasing_subsequences_491.go
@@ -4,7 +4,51 @@ package backtracking
 491. Increasing Subsequences
 https://leetcode.com/problems/increasing-subsequences/
 */
-func findSubsequences(nums []int) [][]int {
+func findSubsequencesRecursiveBacktracking(nums []int) [][]int {
+	res := [][]int{}
+
+	var hasSameElement = func(a [][]int, b []int) bool {
+		for _, el := range a {
+			foundDuplicate := true
+			if len(el) != len(b) {
+				foundDuplicate = false
+				continue
+			}
+			for i := 0; i < len(el); i++ {
+				if el[i] != b[i] {
+					foundDuplicate = false
+					break
+				}
+			}
+			if foundDuplicate {
+				return true
+			}
+		}
+
+		return false
+	}
+
+	var backtracking func(startIndex int, current []int)
+	backtracking = func(startIndex int, current []int) {
+		if startIndex >= len(nums) {
+			if len(current) > 1 && !hasSameElement(res, current) {
+				cpy := make([]int, len(current))
+				copy(cpy, current)
+				res = append(res, cpy)
+			}
+			return
+		}
+		backtracking(startIndex+1, current)
+		if len(current) == 0 || current[len(current)-1] <= nums[startIndex] {
+			backtracking(startIndex+1, append(current, nums[startIndex]))
+		}
+	}
+
+	backtracking(0, nil)
+	return res
+}
+
+func findSubsequencesIterativeBacktracking(nums []int) [][]int {
 	res := [][]int{}
 	cur := []int{}
 
@@ -33,6 +77,10 @@ func findSubsequences(nums []int) [][]int {
 	return res
 }
 
-func FindSubsequences(nums []int) [][]int {
-	return findSubsequences(nums)
+func FindSubsequencesIterativeBacktracking(nums []int) [][]int {
+	return findSubsequencesIterativeBacktracking(nums)
+}
+
+func FindSubsequencesRecursiveBacktracking(nums []int) [][]int {
+	return findSubsequencesRecursiveBacktracking(nums)
 }
