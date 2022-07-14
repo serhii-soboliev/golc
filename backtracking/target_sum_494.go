8de9852b8b4452309587c4033a981fee4bc0c076
sergiy.sobolev@gmail.com <sergiy.sobolev@gmail.com>
2022-07-14 23:59:32 +0300

    [494. Target Sum] memoization backtracking

--- backtracking/target_sum_494.go
+++ backtracking/target_sum_494.go
@@ -4,8 +4,45 @@ package backtracking
 494. Target Sum
 https://leetcode.com/problems/target-sum/
 */
+func findTargetSumWaysMemoBacktracking(nums []int, target int) int {
+	l := len(nums)
+	total := 0
+	for _, n := range nums {
+		total += n
+	}
 
-func findTargetSumWays(nums []int, target int) int {
+	memo := make([][]int, l)
+	min := 0 - total - 1
+	for i := range memo {
+		memo[i] = make([]int, 2*total+1)
+		for j := 0; j < 2*total+1; j++ {
+			memo[i][j] = min
+		}
+	}
+
+	var backtracking func(int, int) int
+
+	backtracking = func(startIndex int, sum int) int {
+		if startIndex == l {
+			if sum == target {
+				return 1
+			} else {
+				return 0
+			}
+		}
+		if memo[startIndex][total+sum] != min {
+			return memo[startIndex][total+sum]
+		}
+		add := backtracking(startIndex+1, sum+nums[startIndex])
+		subtract := backtracking(startIndex+1, sum-nums[startIndex])
+		memo[startIndex][total+sum] = add + subtract
+		return memo[startIndex][total+sum]
+	}
+
+	return backtracking(0, 0)
+}
+
+func findTargetSumWaysBruteForce(nums []int, target int) int {
 	count := 0
 	var backtracking func(int, int)
 	backtracking = func(startIndex int, sum int) {
@@ -23,6 +60,10 @@ func findTargetSumWays(nums []int, target int) int {
 	return count
 }
 
-func FindTargetSumWays(nums []int, target int) int {
-	return findTargetSumWays(nums, target)
+func FindTargetSumWaysMemoBacktracking(nums []int, target int) int {
+	return findTargetSumWaysMemoBacktracking(nums, target)
+}
+
+func FindTargetSumWaysBruteForce(nums []int, target int) int {
+	return findTargetSumWaysBruteForce(nums, target)
 }
