c4cf3249d06256cbc3a8037c0f6e2d72ed9c8b55
sergiy.sobolev@gmail.com <sergiy.sobolev@gmail.com>
2022-07-27 12:18:15 +0300

    [816. Ambiguous Coordinates] backtracking solution

--- pkg/backtracking/816.ambiguous_coordinates.go
+++ pkg/backtracking/816.ambiguous_coordinates.go
@@ -1,11 +1,48 @@
 package backtracking
 
+import "fmt"
+
 /*
 816. Ambiguous Coordinates
 https://leetcode.com/problems/ambiguous-coordinates/
 */
-func ambiguousCoordinates(s string) []string {
-	return []string{}
+func ambiguousCoordinates(st string) []string {
+	res := []string{}
+
+	isNotLegal := func(s string) bool {
+		return !(len(s) == 1 || s[0] != '0' || s[len(s)-1] != '0')
+	}
+
+	makeCoordinates := func(s string) []string {
+		switch {
+			case len(s) == 1 || s[len(s)-1] == '0':
+				return []string{s}
+			case s[0] == '0':
+				return []string{fmt.Sprintf("0.%s", s[1:])}
+			default:
+				result := make([]string, 1, len(s))
+				result[0] = s
+				for i := 1; i < len(s); i++ {
+					result = append(result, fmt.Sprintf("%s.%s", s[:i], s[i:]))
+				}
+				return result
+			}
+	}
+
+	for i:=2; i<len(st)-1; i++ {
+		left, right := st[1:i], st[i:len(st)-1]
+		if isNotLegal(left) || isNotLegal(right) {
+			continue
+		}
+		leftC, rightC := makeCoordinates(left), makeCoordinates(right)
+		for _, l := range leftC {
+			for _, r := range rightC {
+				res = append(res, []string{fmt.Sprintf("(%s, %s)", l, r)} ...)
+			}
+		}
+	}
+
+	return res
 }
 
 func AmbiguousCoordinates(s string) []string {
--- pkg/backtracking/816.ambiguous_coordinates_test.go
+++ pkg/backtracking/816.ambiguous_coordinates_test.go
@@ -8,10 +8,33 @@ import (
 )
 
 func TestAmbiguousCoordinates1(t *testing.T) {
-	result := bt.AmbiguousCoordinates("(123)")
-	 assert.Len(t, result, 0)
-	// assert.Contains(t, result, "(1, 2.3)")
-	// assert.Contains(t, result, "(1, 23)")
-	// assert.Contains(t, result, "(1.2, 3)")
-	// assert.Contains(t, result, "(12, 3)")
+	r := bt.AmbiguousCoordinates("(123)")
+	assert.Len(t, r, 4)
+	assert.Contains(t, r, "(1, 2.3)")
+	assert.Contains(t, r, "(1, 23)")
+	assert.Contains(t, r, "(1.2, 3)")
+	assert.Contains(t, r, "(12, 3)")
+}
+
+func TestAmbiguousCoordinates2(t *testing.T) {
+	r := bt.AmbiguousCoordinates("(000)")
+	assert.Len(t, r, 0)
+}
+
+func TestAmbiguousCoordinates3(t *testing.T) {
+	r := bt.AmbiguousCoordinates("(00011)")
+	assert.Len(t, r, 2)
+	assert.Contains(t, r, "(0, 0.011)")
+	assert.Contains(t, r, "(0.001, 1)")
+}
+
+func TestAmbiguousCoordinates4(t *testing.T) {
+	r := bt.AmbiguousCoordinates("(0123)")
+	assert.Len(t, r, 6)
+	assert.Contains(t, r, "(0, 1.23)")
+	assert.Contains(t, r, "(0, 12.3)")
+	assert.Contains(t, r, "(0, 123)")
+	assert.Contains(t, r, "(0.1, 2.3)")
+	assert.Contains(t, r, "(0.1, 23)")
+	assert.Contains(t, r, "(0.12, 3)")
 }
