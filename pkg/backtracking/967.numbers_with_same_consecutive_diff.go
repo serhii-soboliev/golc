a996f26371b379bdfe0c89f77ad7f01af76de56f
sergiy.sobolev@gmail.com <sergiy.sobolev@gmail.com>
2022-07-31 19:06:54 +0300

    [967. Numbers With Same Consecutive Differences] Backtracking

--- pkg/backtracking/967.numbers_with_same_consecutive_diff.go
+++ pkg/backtracking/967.numbers_with_same_consecutive_diff.go
@@ -1,12 +1,35 @@
 package backtracking
 
+import "math"
 /*
 967. Numbers With Same Consecutive Differences
 https://leetcode.com/problems/numbers-with-same-consecutive-differences/
 */
-
 func numsSameConsecDiff(n int, k int) []int {
-	return [] int {}
+	res := []int{}
+	highest := int(math.Pow10(n))
+	lowest := int(math.Pow10(n-1))
+
+	var backtrack func(number int, last int)
+	backtrack = func(number int, last int) {
+		if lowest <= number && number < highest {
+			res = append(res, number)
+			return
+		}	
+		lower := last - k 
+		if lower >= 0 {
+			backtrack(number * 10 + lower, lower)
+		}
+		higher := last + k
+		if higher <= 9 && higher != lower{
+			backtrack(number * 10 + higher, higher)
+		}
+	}
+
+	for i:=1; i<=9; i++ {
+		backtrack(i, i)
+	}
+	return res
 }
 
 func NumsSameConsecDiff(n int, k int) []int {
--- pkg/backtracking/967.numbers_with_same_consecutive_diff_test.go
+++ pkg/backtracking/967.numbers_with_same_consecutive_diff_test.go
@@ -14,7 +14,17 @@ func TestNumsSameConsecDiff1(t *testing.T)  {
 	assert.ElementsMatch(t, []int {181,292,707,818,929}, r)
 }
 
-func TestNumsSameConsecDiff12(t *testing.T)  {
+func TestNumsSameConsecDiff2(t *testing.T)  {
 	r := bt.NumsSameConsecDiff(2, 1)
 	assert.ElementsMatch(t, []int {10,12,21,23,32,34,43,45,54,56,65,67,76,78,87,89,98}, r)
+}
+
+func TestNumsSameConsecDiff3(t *testing.T)  {
+	r := bt.NumsSameConsecDiff(2, 0)
+	assert.ElementsMatch(t, []int {11,22,33,44,55,66,77,88,99}, r)
+}
+
+func TestNumsSameConsecDiff4(t *testing.T)  {
+	r := bt.NumsSameConsecDiff(3, 1)
+	assert.ElementsMatch(t, []int {101,121,123,210,212,232,234,321,323,343,345,432,434,454,456,543,545,565,567,654,656,676,678,765,767,787,789,876,878,898,987,989}, r)
 }
