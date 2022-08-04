f28affff4a260f7d19fcf3045e18b42f678a2630
sergiy.sobolev@gmail.com <sergiy.sobolev@gmail.com>
2022-08-04 22:43:28 +0300

    [858. Mirror Reflection] GCD & LCM

--- pkg/math/858.mirror_reflection.go
+++ pkg/math/858.mirror_reflection.go
@@ -4,12 +4,38 @@ package math
 858. Mirror Reflection
 https://leetcode.com/problems/mirror-reflection/
 */
+
+func gcd(a, b int) int {
+	if a == 0 || a == b { return b }
+	if b == 0 { return a }
+	if a > b {
+		return gcd(a - b, b)
+	} else {
+		return gcd(a, b - a)
+	}
+}
+
+func lcm(a, b int) int {
+	return a*b / gcd(a, b)
+}
+
+func mirrorReflectionLCM(p int, q int) int {
+	lcm := lcm(p, q)
+	m := lcm / p
+	n := lcm / q
+	return 1 + m % 2 - n % 2
+}
+
 func mirrorReflectionMath(p int, q int) int {
 	for p % 2 == 0 && q % 2 == 0 {
 		p /= 2
 		q /= 2
 	}
-	return 1 - p%2 + q % 2
+	return 1 - p % 2 + q % 2
+}
+
+func MirrorReflectionLCM(p int, q int) int {
+	return mirrorReflectionLCM(p,q)
 }
 
 func MirrorReflectionMath(p int, q int) int {
--- pkg/math/858.mirror_reflection_test.go
+++ pkg/math/858.mirror_reflection_test.go
@@ -12,3 +12,7 @@ func TestMirrorReflectionMath(t *testing.T) {
 	assert.Equal(t, 1, lcmath.MirrorReflectionMath(3, 1))
 }
 
+func TestMirrorReflectionLCM(t *testing.T) {
+	assert.Equal(t, 2, lcmath.MirrorReflectionLCM(2, 1))
+	assert.Equal(t, 1, lcmath.MirrorReflectionLCM(3, 1))
+}
