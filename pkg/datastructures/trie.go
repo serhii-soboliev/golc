2d783aafc1fc104def9a03b7672ba36002594756
sergiy.sobolev@gmail.com <sergiy.sobolev@gmail.com>
2022-08-05 09:41:11 +0300

    fix compilaiton errors

--- pkg/datastructures/trie.go
+++ pkg/datastructures/trie.go
@@ -1,30 +1,29 @@
-type Trie struct {
-    
-}
-
 package datastructures
 
 /*
 208. Implement Trie (Prefix Tree)
 https://leetcode.com/problems/implement-trie-prefix-tree/
 */
+type Trie struct {
+	children [26]*Trie
+    isEnd bool;
+}
 
 func Constructor() Trie {
-	children [26]*Trie
-    isEnd bool
+	return Trie{}
 }
 
 
 func (this *Trie) Insert(word string)  {
-    
+	
 }
 
 
 func (this *Trie) Search(word string) bool {
-    
+	return false
 }
 
 
 func (this *Trie) StartsWith(prefix string) bool {
-    
+	return false
 }
