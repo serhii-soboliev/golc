dbc47fea80529b7393078e66208060e1119d59dc
sergiy.sobolev@gmail.com <sergiy.sobolev@gmail.com>
2022-08-05 09:37:47 +0300

    [377. Combination Sum IV]

---
+++ pkg/backtracking/212.word_search _2.go
@@ -0,0 +1,11 @@
+package backtracking
+
+/*
+212. Word Search II
+https://leetcode.com/problems/word-search-ii/
+*/
+
+func findWords(board [][]byte, words []string) []string {
+	result := [] string {}
+	return result
+}
---
+++ pkg/datastructures/trie.go
@@ -0,0 +1,30 @@
+type Trie struct {
+    
+}
+
+package datastructures
+
+/*
+208. Implement Trie (Prefix Tree)
+https://leetcode.com/problems/implement-trie-prefix-tree/
+*/
+
+func Constructor() Trie {
+	children [26]*Trie
+    isEnd bool
+}
+
+
+func (this *Trie) Insert(word string)  {
+    
+}
+
+
+func (this *Trie) Search(word string) bool {
+    
+}
+
+
+func (this *Trie) StartsWith(prefix string) bool {
+    
+}
