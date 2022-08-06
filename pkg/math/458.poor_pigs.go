7c61f1eb16a0fe4a7503e1614e4b1e901cf5d3f5
sergiy.sobolev@gmail.com <sergiy.sobolev@gmail.com>
2022-08-06 11:14:35 +0300

    [458. Poor Pigs] Formula solution

--- pkg/math/458.poor_pigs.go
+++ pkg/math/458.poor_pigs.go
@@ -1,5 +1,6 @@
 package math
 
+import "math"
 
 /*
 458. Poor Pigs
@@ -14,6 +15,14 @@ func poorPigs(buckets int, minutesToDie int, minutesToTest int) int {
 		n *= testNum
 	}
 	return pigs
+}
+
+func poorPigsM(buckets int, minutesToDie int, minutesToTest int) int {
+	return int(math.Ceil(math.Log(float64(buckets)) / math.Log(float64(minutesToTest/minutesToDie+1))))	
+}
+
+func PoorPigsM(buckets int, minutesToDie int, minutesToTest int) int {
+	return poorPigsM(buckets, minutesToDie, minutesToTest)
 }
 
 func PoorPigs(buckets int, minutesToDie int, minutesToTest int) int {
--- pkg/math/458.poor_pigs_test.go
+++ pkg/math/458.poor_pigs_test.go
@@ -12,3 +12,9 @@ func TestPoorPig(t *testing.T) {
 	assert.Equal(t, 2, lcmath.PoorPigs(4, 15, 15))
 	assert.Equal(t, 2, lcmath.PoorPigs(4, 15, 30))
 }
+
+func TestPoorPigM(t *testing.T) {
+	assert.Equal(t, 5, lcmath.PoorPigsM(1000, 15, 60))
+	assert.Equal(t, 2, lcmath.PoorPigsM(4, 15, 15))
+	assert.Equal(t, 2, lcmath.PoorPigsM(4, 15, 30))
+}
