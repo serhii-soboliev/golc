0d3fe3b998588c775bda62c86d81068c39219744
sergiy.sobolev@gmail.com <sergiy.sobolev@gmail.com>
2022-08-13 22:56:07 +0300

    [30. Substring with Concatenation of All Words] Sliding window

--- pkg/string/30.substring_with_concatenation_of_all_words.go
+++ pkg/string/30.substring_with_concatenation_of_all_words.go
@@ -5,7 +5,7 @@ package string
 https://leetcode.com/problems/substring-with-concatenation-of-all-words/
 */
 
-func findSubstring(s string, words []string) []int {
+func findSubstringMap(s string, words []string) []int {
 
 	dict := make(map[string]int)
 	for _, word := range words {
@@ -27,26 +27,89 @@ func findSubstring(s string, words []string) []int {
 		for k, v := range dict {
 			remaining[k] = v
 		}
-		for j:=i; j<i+l; j+=n {
-			sub := s[j:j+n]
-			if v, ok := remaining[sub]; ok && v > 0{
+		for j := i; j < i+l; j += n {
+			sub := s[j : j+n]
+			if v, ok := remaining[sub]; ok && v > 0 {
 				remaining[sub] = v - 1
-				wordsCount += 1	
+				wordsCount += 1
 			} else {
 				break
-			}	
+			}
 		}
 		return wordsCount == k
 	}
-	
-	for i:=0; i<len(s)-l + 1; i++ {
+
+	for i := 0; i < len(s)-l+1; i++ {
 		if check(i) {
 			result = append(result, i)
-		}		
-	} 
+		}
+	}
 	return result
 }
 
-func FindSubstring(s string, words []string) []int {
-	return findSubstring(s, words)
+func findSubstringSlidingWindow(s string, words []string) []int {
+	dict := make(map[string]int)
+	for _, word := range words {
+		if v, ok := dict[word]; ok {
+			dict[word] = v + 1
+		} else {
+			dict[word] = 1
+		}
+	}
+
+	wordLen := len(words[0])
+	wordsCount := len(words)
+	substringSize := wordsCount * wordLen
+	result := []int{}
+
+	slidingWindow := func(left int) {
+		wordsFound := make(map[string]int)
+		excessWord := false
+		wordsUsed := 0
+		for right := left; right + wordLen <= len(s); right += wordLen {
+			sub := s[right:right+wordLen]
+			if v1, ok1 := dict[sub]; ok1 {
+				for right - left == substringSize || excessWord {
+					leftmostWord := s[left:left+wordLen]
+					left += wordLen
+					wordsFound[leftmostWord] -= 1
+					if wordsFound[leftmostWord] >= dict[leftmostWord] {
+						excessWord = false
+					} else {
+						wordsUsed -= 1
+					}
+				}
+				if v2, ok2 := wordsFound[sub]; ok2 {
+					wordsFound[sub] = v2 + 1
+				} else {
+					wordsFound[sub] = 1	
+				}
+				if wordsFound[sub] <= v1 {
+					wordsUsed += 1
+				} else {
+					excessWord = true
+				}
+				if wordsUsed == wordsCount && !excessWord {
+					result = append(result, left)
+				}
+			} else {
+				wordsFound = make(map[string]int)
+				excessWord = false
+				wordsUsed = 0
+				left = right + wordLen
+			}
+		}	
+	}
+	for i:=0; i<wordLen; i++ {
+		slidingWindow(i)
+	}
+	return result
+}
+
+func FindSubstringSlidingWindow(s string, words []string) []int {
+	return findSubstringSlidingWindow(s, words)
+}
+
+func FindSubstringMap(s string, words []string) []int {
+	return findSubstringMap(s, words)
 }
--- pkg/string/30.substring_with_concatenation_of_all_words_test.go
+++ pkg/string/30.substring_with_concatenation_of_all_words_test.go
@@ -7,19 +7,36 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestFindSubstring1(t *testing.T) {
-	assert.ElementsMatch(t, []int{0,9}, 
-		st.FindSubstring("barfoothefoobarman", []string{"foo","bar"}))
+func TestFindSubstringMap(t *testing.T) {
 
-	assert.Empty(t, 
-		st.FindSubstring("wordgoodgoodgoodbestword", 
-		[]string{"word","good","best","word"}))	
+	assert.ElementsMatch(t, []int{0, 9},
+		st.FindSubstringMap("barfoothefoobarman", []string{"foo", "bar"}))
 
-	assert.ElementsMatch(t, []int{6,9,12}, 
-		st.FindSubstring("barfoofoobarthefoobarman", []string{"bar","foo","the"}))		
+	assert.Empty(t,
+		st.FindSubstringMap("wordgoodgoodgoodbestword",
+			[]string{"word", "good", "best", "word"}))
+
+	assert.ElementsMatch(t, []int{6, 9, 12},
+		st.FindSubstringMap("barfoofoobarthefoobarman", []string{"bar", "foo", "the"}))
+
+	assert.ElementsMatch(t, []int{8},
+		st.FindSubstringMap("wordgoodgoodgoodbestword", []string{"word", "good", "best", "good"}))
+
 }
 
-func TestFindSubstring2(t *testing.T) {
-	assert.ElementsMatch(t, []int{8}, 
-		st.FindSubstring("wordgoodgoodgoodbestword", []string{"word","good","best","good"}))
+func TestFindSubstringSlidinigWindow(t *testing.T) {
+
+	assert.ElementsMatch(t, []int{0, 9},
+		st.FindSubstringSlidingWindow("barfoothefoobarman", []string{"foo", "bar"}))
+
+	assert.Empty(t,
+		st.FindSubstringSlidingWindow("wordgoodgoodgoodbestword",
+			[]string{"word", "good", "best", "word"}))
+
+	assert.ElementsMatch(t, []int{6, 9, 12},
+		st.FindSubstringSlidingWindow("barfoofoobarthefoobarman", []string{"bar", "foo", "the"}))
+
+	assert.ElementsMatch(t, []int{8},
+		st.FindSubstringSlidingWindow("wordgoodgoodgoodbestword", []string{"word", "good", "best", "good"}))
+
 }
