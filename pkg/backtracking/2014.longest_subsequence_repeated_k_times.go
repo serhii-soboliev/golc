68a9e1cd38ebecad0aff60898a4ce13612859a08
sergiy.sobolev@gmail.com <sergiy.sobolev@gmail.com>
2022-08-26 22:57:59 +0300

    [2014. Longest Subsequence Repeated k Times] BFS solution

--- pkg/backtracking/2014.longest_subsequence_repeated_k_times.go
+++ pkg/backtracking/2014.longest_subsequence_repeated_k_times.go
@@ -8,8 +8,8 @@ https://leetcode.com/problems/longest-subsequence-repeated-k-times/
 */
 
 func KCombinations(a []byte, k int) (result [][]byte) {
-	
-	var combBacktrack func(pos int,l int, current []byte)
+
+	var combBacktrack func(pos int, l int, current []byte)
 
 	combBacktrack = func(pos int, l int, current []byte) {
 		if l == 0 {
@@ -17,7 +17,7 @@ func KCombinations(a []byte, k int) (result [][]byte) {
 			return
 		}
 		currendIdx := k - l
-		for i:=pos; i<=len(a)-l; i++ {
+		for i := pos; i <= len(a)-l; i++ {
 			cpy := make([]byte, len(current))
 			copy(cpy, current)
 			cpy[currendIdx] = a[i]
@@ -34,12 +34,12 @@ func Perm(a []byte) (result []string) {
 
 	resMap := make(map[string]bool)
 
-	var perm func(a []byte,  i int) 
+	var perm func(a []byte, i int)
 
 	perm = func(a []byte, i int) {
 		if i > len(a) {
 			resMap[string(a)] = true
-			return 
+			return
 		}
 		perm(a, i+1)
 		for j := i + 1; j < len(a); j++ {
@@ -60,10 +60,10 @@ func doesMeetKTimes(a, b string, times int) bool {
 	l := len(a)
 	totalTimes := times * l
 	currentTimes := 0
-	for i:=0; i<len(b); i++ {
-		if b[i] == a[currentTimes % l] {
+	for i := 0; i < len(b); i++ {
+		if b[i] == a[currentTimes%l] {
 			currentTimes += 1
-			if currentTimes == totalTimes { 
+			if currentTimes == totalTimes {
 				return true
 			}
 		}
@@ -78,31 +78,85 @@ func PossibleKLenSubStrings(a []byte, k int) (result []string) {
 	}
 	sort.Sort(sort.Reverse(sort.StringSlice(result)))
 	return
-} 
+}
 
-func longestSubsequenceRepeatedK(s string, k int) string {
+func PossibleCharsForKLenStrings(s string, n int) (possibleChars []byte) {
+
 	chars := make([]int, 26)
 	for _, c := range s {
-		chars[c - 'a'] += 1
+		chars[c-'a'] += 1
 	}
 
-	possibleCharsForKLenStrings := func(n int) (possibleChars []byte) {
-		for i, c := range chars {
-			if c >= n {
-				times := c / n
-				for j:=0; j<times; j++ {
-					possibleChars = append(possibleChars, byte(i + 'a'))
-				}	
+	for i, c := range chars {
+		if c >= n {
+			times := c / n
+			for j := 0; j < times; j++ {
+				possibleChars = append(possibleChars, byte(i+'a'))
 			}
 		}
+	}
+	return
+}
 
-		return
+func PossibleCharsForKLenStringsWoDuplicates(s string, n int) (possibleChars []byte) {
+
+	chars := make([]int, 26)
+	for _, c := range s {
+		chars[c-'a'] += 1
 	}
 
-	possibleChars := possibleCharsForKLenStrings(k)
+	for i, c := range chars {
+		if c >= n {
+			possibleChars = append(possibleChars, byte(i+'a'))
+		}
+	}
+	return
+}
 
-	for i:=len(possibleChars); i>=1; i-- {
-		found := false
+func IsValid(a string, b string, n int) bool{
+		i, cnter := 0,0
+		for _, c:= range b {
+			if a[i] == byte(c) { 
+				i += 1 
+			}	
+			if i==len(a) {
+				cnter += 1
+				i = 0
+			}
+			if cnter==n { 
+				return true
+			}
+		}
+		return false
+}
+
+func longestSubsequenceRepeatedK(s string, k int) string {
+
+	cans := PossibleCharsForKLenStringsWoDuplicates(s, k)
+	
+	result := ""
+	q := []string{""}
+
+	for len(q) > 0 {
+		t := q[0]
+		q = q[1:]
+		for _, c := range cans {
+			candidate := t + string(c)
+			if doesMeetKTimes(candidate, s, k) {
+				result = candidate 
+				q = append(q, candidate)
+			}
+		}
+	}
+
+	return result
+}
+
+func longestSubsequenceRepeatedKPrimitive(s string, k int) string {
+
+	possibleChars := PossibleCharsForKLenStringsWoDuplicates(s, k)
+
+	for i := len(possibleChars); i >= 1; i-- {
 		possibleSubStrings := PossibleKLenSubStrings(possibleChars, i)
 
 		for _, substr := range possibleSubStrings {
@@ -110,14 +164,16 @@ func longestSubsequenceRepeatedK(s string, k int) string {
 				return substr
 			}
 		}
-		if found {
-			break
-		}
-	}	
 
-	return "" 
+	}
+
+	return ""
 }
 
 func LongestSubsequenceRepeatedK(s string, k int) string {
 	return longestSubsequenceRepeatedK(s, k)
+}
+
+func LongestSubsequenceRepeatedKPrimitive(s string, k int) string {
+	return longestSubsequenceRepeatedKPrimitive(s, k)
 }
--- pkg/backtracking/2014.longest_subsequence_repeated_k_times_test.go
+++ pkg/backtracking/2014.longest_subsequence_repeated_k_times_test.go
@@ -7,6 +7,18 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestLongestSubsequenceRepeatedKPrimitive1(t *testing.T) {
+	assert.Equal(t, "let", bt.LongestSubsequenceRepeatedKPrimitive("letsleetcode", 2))
+}
+
+func TestLongestSubsequenceRepeatedKPrimitive2(t *testing.T) {
+	assert.Equal(t, "b", bt.LongestSubsequenceRepeatedKPrimitive("bb", 2))
+}
+
+func TestLongestSubsequenceRepeatedKPrimitive3(t *testing.T) {
+	assert.Equal(t, "", bt.LongestSubsequenceRepeatedKPrimitive("ab", 2))
+}
+
 func TestLongestSubsequenceRepeatedK1(t *testing.T) {
 	assert.Equal(t, "let", bt.LongestSubsequenceRepeatedK("letsleetcode", 2))
 }
@@ -18,4 +30,3 @@ func TestLongestSubsequenceRepeatedK2(t *testing.T) {
 func TestLongestSubsequenceRepeatedK3(t *testing.T) {
 	assert.Equal(t, "", bt.LongestSubsequenceRepeatedK("ab", 2))
 }
-
