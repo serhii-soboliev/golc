6691608e035ab928c196975fb2ed7086de3d7d70
sergiy.sobolev@gmail.com <sergiy.sobolev@gmail.com>
2022-08-27 12:43:48 +0300

    [363. Max Sum of Rectangle No Larger Than K] Refactor

--- pkg/matrix/363.max_sum_rectangle_no_larger_k.go
+++ pkg/matrix/363.max_sum_rectangle_no_larger_k.go
@@ -10,28 +10,26 @@ https://leetcode.com/problems/max-sum-of-rectangle-no-larger-than-k/
 func buildPrefixSum(matrix [][]int) [][]int {
 	m, n := len(matrix), len(matrix[0])
 	result := [][]int {}
-	for i:=0; i<m+1; i++ {
-		result = append(result, make([]int, n))
+	for i:=0; i<m; i++ {
+		result = append(result, make([]int, n+1))
 	}
 	for i:=0; i<m; i++ {
 		for j:=0; j<n; j++ {
-			result[i+1][j] = result[i][j] + matrix[i][j]
+			result[i][j+1] = result[i][j] + matrix[i][j]
 		}
 	}
 	return result
 }
 
 func findRectangleSumClosestToK(prefixSum [][]int, k int) int {
-	m, n := len(prefixSum)-1, len(prefixSum[0])
+	m, n := len(prefixSum), len(prefixSum[0])
 	result := math.MinInt
-	for top := 0; top < m; top++ {
-		for bottom := top+1; bottom <= m; bottom++ {
-			for left:=0; left < n; left++ {
+	for left := 0; left < n-1; left++ {
+		for right:=left; right < n-1; right++ {
+			for top:=0; top < m; top++ {
 				subresult := 0
-				for right := left; right < n; right++ {
-					one := prefixSum[bottom][right]
-					two := prefixSum[top][right]	
-					subresult += one - two	
+				for current := top; current < m; current++ {
+					subresult += prefixSum[current][right+1] - prefixSum[current][left]
 					if subresult == k {
 						return subresult
 					}
