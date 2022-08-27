fd50708fa81b965dde0be17f29f22942dcc8201c
sergiy.sobolev@gmail.com <sergiy.sobolev@gmail.com>
2022-08-27 22:49:32 +0300

    [2305. Fair Distribution of Cookies] Backtracking

---
+++ pkg/backtracking/2305.fair_distribution_cookies.go
@@ -0,0 +1,46 @@
+package backtracking
+
+import "math"
+
+/*
+2305. Fair Distribution of Cookies
+https://leetcode.com/problems/fair-distribution-of-cookies/
+*/
+
+func distributeCookies(cookies []int, k int) int {
+
+	n := len(cookies)
+
+	unfairness := math.MaxInt
+
+	var backtrack func(pos int, receivedCookies []int, currentUnfairness int) 
+	
+	backtrack = func(pos int, receivedCookies []int, currentUnfairness int) {
+		if pos == n {
+			if currentUnfairness < unfairness {
+				unfairness = currentUnfairness
+			}
+			return
+		}
+		currentCookies := cookies[pos]
+		for i:=0; i<k; i++ {
+			receivedCookies[i] += currentCookies
+			backtrack(pos+1, receivedCookies, max(receivedCookies[i], currentUnfairness))
+			receivedCookies[i] -= currentCookies	
+		}
+	}
+
+	backtrack(0, make([]int, k), 0)
+	return unfairness
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func DistributeCookies(cookies []int, k int) int {
+	return distributeCookies(cookies, k)
+}
---
+++ pkg/backtracking/2305.fair_distribution_cookies_test.go
@@ -0,0 +1,16 @@
+package backtracking_test
+
+import (
+	bt "golc/pkg/backtracking"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDistributeCookies1(t *testing.T) {
+	assert.Equal(t, 31, bt.DistributeCookies([]int{8,15,10,20,8}, 2))
+}
+
+func TestDistributeCookies2(t *testing.T) {
+	assert.Equal(t, 7, bt.DistributeCookies([]int{6,1,3,2,2,4,1,2}, 3))
+}
