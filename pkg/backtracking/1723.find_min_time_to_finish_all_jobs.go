e23502ff42014bdb3900fb2353f3523928a1b989
sergiy.sobolev@gmail.com <sergiy.sobolev@gmail.com>
2022-09-05 18:52:52 +0300

    [1723. Find Minimum Time to Finish All Jobs] Optimization

--- pkg/backtracking/1723.find_min_time_to_finish_all_jobs.go
+++ pkg/backtracking/1723.find_min_time_to_finish_all_jobs.go
@@ -1,33 +1,38 @@
 package backtracking
 
-import "math"
+import "sort"
 
 /*
 1723. Find Minimum Time to Finish All Jobs
 https://leetcode.com/problems/find-minimum-time-to-finish-all-jobs/
 */
 
+const MaxInt = int(^uint(0) >> 1)
+
 func minimumTimeRequired(jobs []int, k int) int {
 	n := len(jobs)
-	result := math.MaxInt
-
+	result := MaxInt
+	sort.Sort(sort.Reverse(sort.IntSlice(jobs)))
 	workerTimes := make([]int, k)
 	
 	var backtrack func(pos int, curMax int) 
 
 	backtrack = func(pos int, curMax int) {
 		if pos == n {
-			result = minInt(result, curMax)
+			if result > curMax {
+				result = curMax
+			}
 			return
 		}
-		
+
+		jobDuration := jobs[pos]
 		uniqueWorkerTimes := make(map[int]bool)
 		for i:=0; i<k; i++ {
 			if _, ok := uniqueWorkerTimes[workerTimes[i]]; ok {
 				continue;
 			}
 			uniqueWorkerTimes[workerTimes[i]] = true
-			workerTimes[i] += jobs[pos]
+			workerTimes[i] += jobDuration
 			oldCurMax := curMax
 			if curMax < workerTimes[i] {
 				curMax = workerTimes[i]
@@ -35,8 +40,11 @@ func minimumTimeRequired(jobs []int, k int) int {
 			if curMax < result {
 				backtrack(pos+1, curMax)
 			}
-			workerTimes[i] -= jobs[pos]
+			workerTimes[i] -= jobDuration
 			curMax = oldCurMax
+			if workerTimes[i] == 0 {
+				break
+			}
 		}
 
 	}
@@ -45,12 +53,6 @@ func minimumTimeRequired(jobs []int, k int) int {
 	return result
 }
 
-func minInt(a,b int) int {
-	if a < b {
-		return a
-	}
-	return b
-}
 
 func MinimumTimeRequired(jobs []int, k int) int {
 	return minimumTimeRequired(jobs, k)
