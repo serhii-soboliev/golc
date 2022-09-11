d6428c7b5faf84597df0ffb9355596f642dd94f4
sergiy.sobolev@gmail.com <sergiy.sobolev@gmail.com>
2022-09-11 14:20:07 +0300

    [188. Best Time to Buy and Sell Stock IV] DP

--- pkg/leetcode/dynamicprogramming/188.best_time_buy_sell_stock4.go
+++ pkg/leetcode/dynamicprogramming/188.best_time_buy_sell_stock4.go
@@ -5,12 +5,72 @@ package dynamicprogramming
 https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iv/
 */
 
+const MinInt = -int(^uint(0)>>1) - 1
+
+func maxProfitDPSlow(k int, prices []int) int {
+
+	max := func(a, b int) int {
+		if a > b {
+			return a
+		}
+		return b
+	}
+
+	numDays := len(prices)
+	if k == 0 || numDays == 0 {
+		return 0
+	}
+	numTransactions := k + 1
+	t := make([][]int, numTransactions)
+	for i := 0; i < numTransactions; i++ {
+		t[i] = make([]int, numDays)
+	}
+	for transaction := 1; transaction < numTransactions; transaction++ {
+		for day := 1; day < numDays; day++ {
+			maxVal := t[transaction][day-1]
+			for m:=0; m<day; m++ {
+				maxVal = max(maxVal, prices[day] - prices[m] + t[transaction-1][m])	
+			}
+			t[transaction][day] = maxVal
+		}
+	}
+	return t[numTransactions-1][numDays-1]
+}
+
+func maxProfitDP(k int, prices []int) int {
+
+	max := func(a, b int) int {
+		if a > b {
+			return a
+		}
+		return b
+	}
+
+	numDays := len(prices)
+	if k == 0 || numDays == 0 {
+		return 0
+	}
+	numTransactions := k + 1
+	t := make([][]int, numTransactions)
+	for i := 0; i < numTransactions; i++ {
+		t[i] = make([]int, numDays)
+	}
+	for transaction := 1; transaction < numTransactions; transaction++ {
+		maxDiff := -prices[0]
+		for day := 1; day < numDays; day++ {
+			t[transaction][day] = max(t[transaction][day-1], prices[day]+maxDiff)
+			maxDiff = max(maxDiff, t[transaction-1][day]-prices[day])
+		}
+	}
+	return t[numTransactions-1][numDays-1]
+}
+
 func maxProfitRecursion(k int, prices []int) int {
 
 	max := func(a, b int) int {
 		if a > b {
 			return a
-		} 
+		}
 		return b
 	}
 
@@ -18,12 +78,12 @@ func maxProfitRecursion(k int, prices []int) int {
 	memo := make(map[bool][][]int)
 	memo[true] = make([][]int, k+1)
 	memo[false] = make([][]int, k+1)
-	for i:=0;i<k+1;i++ {
-		memo[true][i] = make([]int, n)	
-		memo[false][i] = make([]int, n)	
-		for j:=0; j<n; j++ {
-			memo[false][i][j] = -1		
-			memo[true][i][j] = -1		
+	for i := 0; i < k+1; i++ {
+		memo[true][i] = make([]int, n)
+		memo[false][i] = make([]int, n)
+		for j := 0; j < n; j++ {
+			memo[false][i][j] = -1
+			memo[true][i][j] = -1
 		}
 	}
 
@@ -38,9 +98,9 @@ func maxProfitRecursion(k int, prices []int) int {
 		}
 		sum := recursion(pos+1, bought, transCount)
 		if bought {
-			sum = max(sum, recursion(pos+1, false, transCount-1) + prices[pos])	
+			sum = max(sum, recursion(pos+1, false, transCount-1)+prices[pos])
 		} else {
-			sum = max(sum, recursion(pos+1, true, transCount) - prices[pos])
+			sum = max(sum, recursion(pos+1, true, transCount)-prices[pos])
 		}
 		memo[bought][transCount][pos] = sum
 		return sum
@@ -77,7 +137,7 @@ func maxProfitBacktracking(k int, prices []int) int {
 			if s.transactionsCount < 2*k {
 				s.isBought = false
 				s.profit += price
-				if s.profit < 0 { 
+				if s.profit < 0 {
 					return
 				}
 				s.transactionsCount += 1
@@ -98,6 +158,14 @@ func maxProfitBacktracking(k int, prices []int) int {
 	search(State{0, false, 0, 0})
 
 	return result
+}
+
+func MaxProfitDPSlow4(k int, prices []int) int {
+	return maxProfitDPSlow(k, prices)
+}
+
+func MaxProfitDP4(k int, prices []int) int {
+	return maxProfitDP(k, prices)
 }
 
 func MaxProfitRecursion4(k int, prices []int) int {
--- pkg/leetcode/dynamicprogramming/188.best_time_buy_sell_stock4_test.go
+++ pkg/leetcode/dynamicprogramming/188.best_time_buy_sell_stock4_test.go
@@ -7,6 +7,30 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestMaxProfitDPSlow41(t *testing.T) {
+	assert.Equal(t, 2, dp.MaxProfitDPSlow4(2, []int{2, 4, 1}))
+}
+
+func TestMaxProfitSlow42(t *testing.T) {
+	assert.Equal(t, 7, dp.MaxProfitDP4(2, []int{3, 2, 6, 5, 0, 3}))
+}
+
+func TestMaxProfitSlow43(t *testing.T) {
+	assert.Equal(t, 6, dp.MaxProfitDP4(2, []int{3, 3, 5, 0, 0, 3, 1, 4}))
+}
+
+func TestMaxProfit41(t *testing.T) {
+	assert.Equal(t, 2, dp.MaxProfitDP4(2, []int{2, 4, 1}))
+}
+
+func TestMaxProfit42(t *testing.T) {
+	assert.Equal(t, 7, dp.MaxProfitDP4(2, []int{3, 2, 6, 5, 0, 3}))
+}
+
+func TestMaxProfit43(t *testing.T) {
+	assert.Equal(t, 6, dp.MaxProfitDP4(2, []int{3, 3, 5, 0, 0, 3, 1, 4}))
+}
+
 func TestMaxProfitMaxProfitRecursion41(t *testing.T) {
 	assert.Equal(t, 2, dp.MaxProfitRecursion4(2, []int{2, 4, 1}))
 }
