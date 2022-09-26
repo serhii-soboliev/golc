4baf608b8227dd4a66f3c5abcabab8676c98d2d0
sergiy.sobolev@gmail.com <sergiy.sobolev@gmail.com>
2022-09-27 01:05:45 +0300

    [113. Path Sum II] Backtracking

--- pkg/leetcode/tree/113.path_sum_2.go
+++ pkg/leetcode/tree/113.path_sum_2.go
@@ -9,30 +9,31 @@ func pathSum(root *TreeNode, targetSum int) [][]int {
 
 	result := [][]int{}
 
+	path := []int{}
+
 	isLeaf := func(n *TreeNode) bool {
 		return n.Left == nil && n.Right == nil
 	}
 
-	var dfs func(path []int, s int, n *TreeNode)
+	var dfs func( s int, n *TreeNode)
 
-	dfs = func(path []int, s int, n *TreeNode){
+	dfs = func( s int, n *TreeNode){
 		if n == nil {
 			return
 		}
-		path = append(append([]int{}, path ...), n.Val)
+		path = append(path, n.Val)
 		newS := s - n.Val
 		if isLeaf(n) {
 			if newS == 0 {
-				result = append(result, path)
+				result = append(result, append([]int{},path...))
 			}
-			return
 		}
 		
-		dfs(path, newS, n.Left)
-		dfs(path, newS, n.Right)
-		
+		dfs(newS, n.Left)
+		dfs(newS, n.Right)
+		path = path[:len(path)-1]
 	}
 
-	dfs([]int{}, targetSum, root)
+	dfs(targetSum, root)
 	return result
 }
