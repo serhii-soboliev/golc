d92ec6d20647ba522fa4a7ef6cdfd711d0eea430
sergiy.sobolev@gmail.com <sergiy.sobolev@gmail.com>
2022-10-05 00:43:04 +0300

    Int priority queue r

--- pkg/leetcode/datastructures/pq/int_pq.go
+++ pkg/leetcode/datastructures/pq/int_pq.go
@@ -2,36 +2,56 @@ package pq
 
 import "container/heap"
 
-type PriorityQueue []int
-
-func (pq PriorityQueue) Len() int           { return len(pq) }
-func (pq PriorityQueue) Less(i, j int) bool { return pq[i] > pq[j] }
-func (pq PriorityQueue) Swap(i, j int)      { pq[i], pq[j] = pq[j], pq[i] }
-
-func (pq *PriorityQueue) Push(x any) {
-	*pq = append(*pq, x.(int))
+type PriorityQueue struct {
+	innerPq InnerPq
 }
 
-func (pq *PriorityQueue) Pop() any {
-	old := *pq
+func NewPriorityQueue() PriorityQueue {
+	iPQ := &InnerPq{}
+	heap.Init(iPQ)
+	return PriorityQueue{innerPq: *iPQ}
+}
+
+func (pq *PriorityQueue) Push(x int) {
+	heap.Push(&pq.innerPq, x)
+}
+
+func (pq *PriorityQueue) Pop() int {
+	return heap.Pop(&pq.innerPq).(int)
+}
+
+func (pq *PriorityQueue) Remove(x int) {
+	pq.innerPq.Remove(x)
+}
+
+type InnerPq []int
+
+func (ipq InnerPq) Len() int           { return len(ipq) }
+func (ipq InnerPq) Less(i, j int) bool { return ipq[i] > ipq[j] }
+func (ipq InnerPq) Swap(i, j int)      { ipq[i], ipq[j] = ipq[j], ipq[i] }
+
+func (ipq *InnerPq) Push(x any) {
+	*ipq = append(*ipq, x.(int))
+}
+
+func (ipq *InnerPq) Pop() any {
+	old := *ipq
 	n := len(old)
 	x := old[n-1]
-	*pq = old[0 : n-1]
+	*ipq = old[0 : n-1]
 	return x
 }
 
-func (pq *PriorityQueue) Remove(toRemove any) {
-	l := pq.Len()
+func (ipq *InnerPq) Remove(toRemove any) {
+	l := ipq.Len()
 	for i := 0; i < l; i++ {
-        n := (*pq)[i]
-        if n == toRemove {
-            (*pq)[i], (*pq)[l-1] = (*pq)[l-1], (*pq)[i]
-            (*pq) = (*pq)[:l-1]
-            i-=1
-			l-=1
-        }
-    }
-    heap.Init(pq)
+		n := (*ipq)[i]
+		if n == toRemove {
+			(*ipq)[i], (*ipq)[l-1] = (*ipq)[l-1], (*ipq)[i]
+			(*ipq) = (*ipq)[:l-1]
+			i -= 1
+			l -= 1
+		}
+	}
+	heap.Init(ipq)
 }
-
-
--- pkg/leetcode/datastructures/pq/int_pq_test.go
+++ pkg/leetcode/datastructures/pq/int_pq_test.go
@@ -8,61 +8,100 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPQ1(t *testing.T) {
-	p := &pq.PriorityQueue{}
+func TestIPQ1(t *testing.T) {
+	p := &pq.InnerPq{}
 	heap.Init(p)
 	heap.Push(p, 1)
 	heap.Push(p, 3)
 	heap.Push(p, 2)
 	heap.Push(p, -1)
 	heap.Push(p, -2)
-	assert.Equal(t, 3,  heap.Pop(p).(int))
-	assert.Equal(t, 2,  heap.Pop(p).(int))
-	assert.Equal(t, 1,  heap.Pop(p).(int))
+	assert.Equal(t, 3, heap.Pop(p).(int))
+	assert.Equal(t, 2, heap.Pop(p).(int))
+	assert.Equal(t, 1, heap.Pop(p).(int))
 	assert.Equal(t, -1, heap.Pop(p).(int))
 	assert.Equal(t, -2, heap.Pop(p).(int))
+}
+
+func TestIPQ2(t *testing.T) {
+	p := &pq.InnerPq{}
+	heap.Init(p)
+	heap.Push(p, 1)
+	heap.Push(p, 3)
+	heap.Push(p, 2)
+	heap.Push(p, -1)
+	heap.Push(p, -2)
+	p.Remove(1)
+	assert.Equal(t, 3, heap.Pop(p).(int))
+	assert.Equal(t, 2, heap.Pop(p).(int))
+	assert.Equal(t, -1, heap.Pop(p).(int))
+	assert.Equal(t, -2, heap.Pop(p).(int))
+}
+
+func TestIPQ3(t *testing.T) {
+	p := &pq.InnerPq{}
+	heap.Init(p)
+	heap.Push(p, 1)
+	heap.Push(p, 3)
+	heap.Push(p, 2)
+	heap.Push(p, -1)
+	heap.Push(p, -2)
+	p.Remove(1)
+	p.Remove(3)
+	assert.Equal(t, 2, heap.Pop(p).(int))
+	assert.Equal(t, -1, heap.Pop(p).(int))
+	assert.Equal(t, -2, heap.Pop(p).(int))
+}
+
+func TestIPQ4(t *testing.T) {
+	p := &pq.InnerPq{}
+	heap.Init(p)
+	heap.Push(p, 1)
+	heap.Push(p, 3)
+	heap.Push(p, 2)
+	heap.Push(p, -1)
+	p.Remove(1)
+	p.Remove(3)
+	assert.Equal(t, 2, heap.Pop(p).(int))
+	assert.Equal(t, -1, heap.Pop(p).(int))
+}
+
+func TestPQ1(t *testing.T) {
+	p := pq.NewPriorityQueue()
+	p.Push(1)
+	p.Push(3)
+	p.Push(2)
+	p.Push(-1)
+	p.Push(-2)
+	assert.Equal(t, 3, p.Pop())
+	assert.Equal(t, 2, p.Pop())
+	assert.Equal(t, 1, p.Pop())
+	assert.Equal(t, -1, p.Pop())
+	assert.Equal(t, -2, p.Pop())
 }
 
 func TestPQ2(t *testing.T) {
-	p := &pq.PriorityQueue{}
-	heap.Init(p)
-	heap.Push(p, 1)
-	heap.Push(p, 3)
-	heap.Push(p, 2)
-	heap.Push(p, -1)
-	heap.Push(p, -2)
+	p := pq.NewPriorityQueue()
+	p.Push(1)
+	p.Push(3)
+	p.Push(2)
+	p.Push(-1)
+	p.Push(-2)
 	p.Remove(1)
-	assert.Equal(t, 3,  heap.Pop(p).(int))
-	assert.Equal(t, 2,  heap.Pop(p).(int))
-	assert.Equal(t, -1, heap.Pop(p).(int))
-	assert.Equal(t, -2, heap.Pop(p).(int))
+	p.Remove(3)
+	assert.Equal(t, 2, p.Pop())
+	assert.Equal(t, -1, p.Pop())
+	assert.Equal(t, -2, p.Pop())
 }
 
 func TestPQ3(t *testing.T) {
-	p := &pq.PriorityQueue{}
-	heap.Init(p)
-	heap.Push(p, 1)
-	heap.Push(p, 3)
-	heap.Push(p, 2)
-	heap.Push(p, -1)
-	heap.Push(p, -2)
-	p.Remove(1)
+	p := pq.NewPriorityQueue()
+	p.Push(3)
+	p.Push(3)
+	p.Push(3)
+	p.Push(-1)
+	p.Push(-2)
 	p.Remove(3)
-	assert.Equal(t, 2,  heap.Pop(p).(int))
-	assert.Equal(t, -1, heap.Pop(p).(int))
-	assert.Equal(t, -2, heap.Pop(p).(int))
+	assert.Equal(t, -1, p.Pop())
+	assert.Equal(t, -2, p.Pop())
 }
-
-func TestPQ4(t *testing.T) {
-	p := &pq.PriorityQueue{}
-	heap.Init(p)
-	heap.Push(p, 1)
-	heap.Push(p, 3)
-	heap.Push(p, 2)
-	heap.Push(p, -1)
-	p.Remove(1)
-	p.Remove(3)
-	assert.Equal(t, 2,  heap.Pop(p).(int))
-	assert.Equal(t, -1, heap.Pop(p).(int))
-}
-
