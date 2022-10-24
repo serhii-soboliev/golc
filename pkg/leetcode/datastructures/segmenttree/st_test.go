fe4ccbfcbf104590568bb9ffb1ba00e44acd2e68
sergiy.sobolev@gmail.com <sergiy.sobolev@gmail.com>
2022-10-25 01:07:55 +0300

    segement tree. Unit tests

--- pkg/leetcode/datastructures/segmenttree/st_test.go
+++ pkg/leetcode/datastructures/segmenttree/st_test.go
@@ -1,7 +1,6 @@
 package segmenttree_test
 
 import (
-
 	st "golc/pkg/leetcode/datastructures/segmenttree"
 	"testing"
 
@@ -9,9 +8,16 @@ import (
 )
 
 func TestSegmentTree1(t *testing.T) {
-	s := st.BuildSegmentTree([]int{1,3,5,7,9,11})
+	s := st.BuildSegmentTree([]int{1, 3, 5, 7, 9, 11})
 	assert.Equal(t, 36, s.Sum(6, 0, 5))
 	assert.Equal(t, 9, s.Sum(6, 0, 2))
 	assert.Equal(t, 27, s.Sum(6, 3, 5))
+	assert.Equal(t, 35, s.Sum(6, 1, 5))
+	assert.Equal(t, 32, s.Sum(6, 2, 5))
+	assert.Equal(t, 25, s.Sum(6, 0, 4))
+	assert.Equal(t, 9, s.Sum(6, 4, 4))
+	assert.Equal(t, 7, s.Sum(6, 3, 3))
+	assert.Equal(t, 1, s.Sum(6, 0, 0))
+	assert.Equal(t, 11, s.Sum(6, 5, 5))
 
 }
