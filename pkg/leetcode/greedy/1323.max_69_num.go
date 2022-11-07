563c7c4676dea2138f832a6ad0de590a3db563fe
sergiy.sobolev@gmail.com <sergiy.sobolev@gmail.com>
2022-11-07 22:54:25 +0200

    [1323. Maximum 69 Number] Greedy

---
+++ pkg/leetcode/greedy/1323.max_69_num.go
@@ -0,0 +1,40 @@
+package greedy
+
+import (
+	"math"
+	"strconv"
+	"strings"
+)
+
+/*
+1323. Maximum 69 Number
+https://leetcode.com/problems/maximum-69-number/
+*/
+
+func maximum69Number (num int) int {
+	digitsToTheRight := -1
+	temp := num
+	digitsPassed := 0
+
+	for temp > 0 {
+		dig := temp % 10
+		if dig == 6 {
+			digitsToTheRight = digitsPassed
+		}
+		digitsPassed += 1
+		temp = temp / 10
+	}
+
+	if digitsToTheRight == -1 {
+		return num
+	} else {
+		return num + 3*int(math.Pow(10, float64(digitsToTheRight)))
+	}
+}
+
+func maximum69Number_2 (num int) int {
+    a := strconv.Itoa(num)
+	r := strings.Replace(a, "6", "9", 1)
+	i, _ := strconv.Atoi(r)
+	return i
+}
